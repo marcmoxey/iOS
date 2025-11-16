e45f365b1d954b8c8114baad8caafef893e7d6a5
marcmoxey <moxeymarc@gmail.com>
2025-11-16 18:22:55 -0500

    Covered Closures

--- 01 - Phase/Swift For Complete Beginners/06 - Closures/6-2.playground/Contents.swift
+++ 01 - Phase/Swift For Complete Beginners/06 - Closures/6-2.playground/Contents.swift
@@ -1,3 +1,35 @@
 import Cocoa
 
 // How to use trailing closure and shorthand syntax
+
+let team = ["Gloria", "Suzanne", "Piper", "Tiffany", "Tasha"]
+
+// dont need the type for parameter and return type
+let sorted = team.sorted { name1, name2 in
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+          
+    return name1 < name2
+    
+}
+print(sorted)
+
+let reverseTeam = team.sorted { $0 > $1 }
+
+
+// When not to use short hand
+    // when closure body too long
+    // when $0 & $1 use muiltple times
+    // when you have 3 or more parameters
+
+
+let tOnly = team.filter({
+    $0.hasPrefix("T")
+})
+print(tOnly)
+
+let uppercaseTeam = team.map { $0.uppercased() }
+print(uppercaseTeam)
