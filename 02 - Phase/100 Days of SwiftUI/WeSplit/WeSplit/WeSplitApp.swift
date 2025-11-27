4f056c91acd5f3ad636b6a545cfb87ea991c19aa
marcmoxey <moxeymarc@gmail.com>
2025-11-27 15:08:59 -0500

    Learning SwfitUI

---
+++ 02 - Phase/100 Days of SwiftUI/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Marc Moxey on 11/27/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
