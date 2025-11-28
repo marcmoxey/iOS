af1ae69d1b3d609f4d3d77c8884459d6cf56cc03
marcmoxey <moxeymarc@gmail.com>
2025-11-28 11:55:10 -0500

    Add demo of WeSplit with new features

---
+++ 02 - Phase/100 Days of SwiftUI/Project2/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Marc Moxey on 11/28/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
