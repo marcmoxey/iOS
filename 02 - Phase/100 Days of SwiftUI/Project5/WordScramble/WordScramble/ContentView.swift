e747606d5435ababd5bb3fe4eba28c8bf8d538cf
marcmoxey <moxeymarc@gmail.com>
2025-12-02 11:32:49 -0500

    Add a toobar button to startGame for new word

--- 02 - Phase/100 Days of SwiftUI/Project5/WordScramble/WordScramble/ContentView.swift
+++ 02 - Phase/100 Days of SwiftUI/Project5/WordScramble/WordScramble/ContentView.swift
@@ -38,129 +38,135 @@ struct ContentView: View {
                 }
             }
             .navigationTitle(rootWord)
-        }
-        .onSubmit(addNewWord)
-        .onAppear(perform: startGame)
-        .alert(errorTitle, isPresented: $showingError) {
-            
-        } message: {
-            Text(errorMessage)
-        }
-    }
-    
-    func addNewWord() {
-        // lowercase and trim the word, to make sure we don't add
-        // duplicate words with case difference
-        let answer = newWord.lowercased().trimmingCharacters(
-            in: .whitespacesAndNewlines)
-        
-        // exit if the remaining string is empty
-        guard answer.count > 0 else { return }
-        
-        guard isOriginal(word: answer) else {
-            wordError(title: "Word used already", message: "Be more original")
-            return
-        }
-        
-        
-        guard isPossible(word: answer) else {
-            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
-            return
-        }
-        
-        guard tooShortWord(word: answer) else {
-            wordError(title: "Word too short" , message: "Word has to be more than 3 characters")
-            return
-        }
-        
-        guard noRootWord(word: answer) else {
-            wordError(title: "No start word", message: "Can't use start word")
-            
-            return
-        }
-        
-        
-        withAnimation {
-            usedWords.insert(answer, at: 0)
-        }
-     
-        newWord = ""
-    }
-    
-    func startGame() {
-        // 1. Find the url for the start.txt in our app bundle
-        if let startWordURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
-            // 2. Load start txt into a string
-            if let startWords = try? String(
-                contentsOf: startWordURL) {
-                // 3. Split the string up into an array of strings, splitting on the line break
-                let allWords = startWords.components(separatedBy: "\n")
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    Button("Start Game") {
+                        startGame()
+                    }
+                }
+            }
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
                 
-                // 4. Pick one random word, or use "silkworm as a sensible default
-                rootWord = allWords.randomElement() ?? "silkworm"
-                
-                // if we are here everything has worked, so we can exit
-                return
+            } message: {
+                Text(errorMessage)
             }
         }
-        // if were are *here* there was a problem trigger a crash and report the error
-        fatalError("Could not load start.txt from bundle")
     }
-    
-    func tooShortWord(word: String) -> Bool {
-        if word.count < 3 {
-            return false
+        func addNewWord() {
+            // lowercase and trim the word, to make sure we don't add
+            // duplicate words with case difference
+            let answer = newWord.lowercased().trimmingCharacters(
+                in: .whitespacesAndNewlines)
+            
+            // exit if the remaining string is empty
+            guard answer.count > 0 else { return }
+            
+            guard isOriginal(word: answer) else {
+                wordError(title: "Word used already", message: "Be more original")
+                return
+            }
+            
+            
+            guard isPossible(word: answer) else {
+                wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+                return
+            }
+            
+            guard tooShortWord(word: answer) else {
+                wordError(title: "Word too short" , message: "Word has to be more than 3 characters")
+                return
+            }
+            
+            guard noRootWord(word: answer) else {
+                wordError(title: "No start word", message: "Can't use start word")
+                
+                return
+            }
+            
+            
+            withAnimation {
+                usedWords.insert(answer, at: 0)
+            }
+            
+            newWord = ""
         }
         
-        return true
-    }
-    
-    func noRootWord(word: String) -> Bool {
-        if word != rootWord {
+        func startGame() {
+            // 1. Find the url for the start.txt in our app bundle
+            if let startWordURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+                // 2. Load start txt into a string
+                if let startWords = try? String(
+                    contentsOf: startWordURL) {
+                    // 3. Split the string up into an array of strings, splitting on the line break
+                    let allWords = startWords.components(separatedBy: "\n")
+                    
+                    // 4. Pick one random word, or use "silkworm as a sensible default
+                    rootWord = allWords.randomElement() ?? "silkworm"
+                    
+                    // if we are here everything has worked, so we can exit
+                    return
+                }
+            }
+            // if were are *here* there was a problem trigger a crash and report the error
+            fatalError("Could not load start.txt from bundle")
+        }
+        
+        func tooShortWord(word: String) -> Bool {
+            if word.count < 3 {
+                return false
+            }
+            
             return true
         }
         
-        return false
-    }
-    
-    
-    func isOriginal(word: String) -> Bool {
-        !usedWords.contains(word)
-    }
-    
-    func isPossible(word: String) -> Bool {
-        var tempWord = rootWord
-        
-        for letter in word {
-            if let pos = tempWord.firstIndex(of: letter) {
-                tempWord.remove(at: pos)
-            } else {
-                return false
+        func noRootWord(word: String) -> Bool {
+            if word != rootWord {
+                return true
             }
+            
+            return false
         }
-        return true
-    }
-    
-    func isReal(word: String) -> Bool {
-        let checker = UITextChecker()
-        let range = NSRange(location: 0, length: word.utf16.count)
-        let misspelledRange = checker.rangeOfMisspelledWord(
-            in: word,
-            range: range,
-            startingAt: 0,
-            wrap: false,
-            language: "en"
-        )
         
-        return misspelledRange.location == NSNotFound
+        
+        func isOriginal(word: String) -> Bool {
+            !usedWords.contains(word)
+        }
+        
+        func isPossible(word: String) -> Bool {
+            var tempWord = rootWord
+            
+            for letter in word {
+                if let pos = tempWord.firstIndex(of: letter) {
+                    tempWord.remove(at: pos)
+                } else {
+                    return false
+                }
+            }
+            return true
+        }
+        
+        func isReal(word: String) -> Bool {
+            let checker = UITextChecker()
+            let range = NSRange(location: 0, length: word.utf16.count)
+            let misspelledRange = checker.rangeOfMisspelledWord(
+                in: word,
+                range: range,
+                startingAt: 0,
+                wrap: false,
+                language: "en"
+            )
+            
+            return misspelledRange.location == NSNotFound
+        }
+        
+        func wordError(title: String, message: String) {
+            errorTitle = title
+            errorMessage = message
+            showingError = true
+           }
     }
-    
-    func wordError(title: String, message: String) {
-        errorTitle = title
-        errorMessage = message
-        showingError = true
-    }
-}
 
 #Preview {
     ContentView()
