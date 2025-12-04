452b38da3eeaaa6eb6f1a50d685d0283b11815fd
marcmoxey <moxeymarc@gmail.com>
2025-12-04 13:18:47 -0500

    Add features to change style depending on item amount and split personal and business into sections

---
+++ 02 - Phase/100 Days of SwiftUI/07Project/iExpense/iExpense/AddView.swift
@@ -0,0 +1,53 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Marc Moxey on 12/4/25.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    var expenses: Expense
+    @Environment(\.dismiss) var dismiss
+    
+    let types = ["Business","Personal"]
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField(
+                    "Amount",
+                    value: $amount,
+                    format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+                )
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(
+                        name: name,
+                        type: type,
+                        amount: amount
+                    )
+                    expenses.items.append(item)
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expense())
+}
--- 02 - Phase/100 Days of SwiftUI/07Project/iExpense/iExpense/ContentView.swift
+++ 02 - Phase/100 Days of SwiftUI/07Project/iExpense/iExpense/ContentView.swift
@@ -11,23 +11,78 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var expenses = Expense()
+    @State private var showingExpense = false
+    
+    
     
     var body: some View {
         NavigationStack {
             List {
-                ForEach(expenses.items, id: \.name) { item in
-                    Text(item.name)
+                // removed id: /.self b/c conforms to idenifiable
+                ForEach(expenses.items) { item in
+                    if item.type == "Personal" {
+                        Section("Personal") {
+                            HStack {
+                                VStack(alignment:.leading){
+                                    Text(item.name)
+                                        .font(.headline)
+                                    Text(item.type)
+                                    
+                                }
+                                Spacer()
+                                let itemPrice = item.amount
+                                //Text(itemPrice, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                                // if amount under $10 do a style
+                                if itemPrice <= 10  {
+                                    Text(itemPrice, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                                        .foregroundStyle(.green)
+                                    // else if amount under $100 do a style
+                                } else if itemPrice > 10 && itemPrice <= 100 {
+                                    Text(itemPrice, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                                        .foregroundStyle(.yellow)
+                                    // else if amount over $100 do a style
+                                } else {
+                                    Text(itemPrice, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                                        .foregroundStyle(.red)
+                                }
+                                
+                            }
+
+                        }
+                        
+                    } else if item.type == "Business" {
+                        Section("Business") {
+                            HStack {
+                                VStack(alignment:.leading){
+                                    Text(item.name)
+                                        .font(.headline)
+                                    Text(item.type)
+                                    
+                                }
+                                Spacer()
+                                Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                                
+                                
+                            }
+                            
+                        }
+                    }
                 }
                 .onDelete(perform: removeItems)
             }
             .toolbar {
                 Button("Add Expense", systemImage: "plus") {
-                    let expense = ExpenseItem(
-                        name: "Test",
-                        type: "Personal",
-                        amount: 5)
-                    expenses.items.append(expense)
+//                    let expense = ExpenseItem(
+//                        
+//                        name: "Test",
+//                        type: "Personal",
+//                        amount: 5)
+//                    expenses.items.append(expense)
+                    showingExpense = true
                     
+                }
+                .sheet(isPresented: $showingExpense) {
+                    AddView(expenses: expenses)
                 }
             }
             .navigationTitle("iExpense")
@@ -39,16 +94,46 @@ struct ContentView: View {
     }
 }
 
-struct ExpenseItem {
+
+
+
+// Identifiable - this type can be identified uniquely
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
     let name: String
     let type: String
     let amount: Double
+    
+
+   
 }
 
 
 @Observable
 class Expense {
-    var items = [ExpenseItem]()
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        // attempts to from whatever item as data object
+        if let savedItems = UserDefaults.standard.data(forKey: "Items")  {
+           // unarchiving the data
+            // [ExpenseItem].self - referring to the type of object
+            if let decodedItem = try? JSONDecoder().decode(
+                [ExpenseItem].self,
+                from: savedItems) {
+                items = decodedItem
+                return
+            }
+        }
+        items = []
+    }
+    
 }
 
 #Preview {
