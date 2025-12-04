9dde31057000d658a6756fca05de836e3ee1333a
marcmoxey <moxeymarc@gmail.com>
2025-12-04 10:08:37 -0500

    Build UI

--- 02 - Phase/100 Days of SwiftUI/07Project/iExpense/iExpense/ContentView.swift
+++ 02 - Phase/100 Days of SwiftUI/07Project/iExpense/iExpense/ContentView.swift
@@ -7,16 +7,48 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    @State private var expenses = Expense()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    let expense = ExpenseItem(
+                        name: "Test",
+                        type: "Personal",
+                        amount: 5)
+                    expenses.items.append(expense)
+                    
+                }
+            }
+            .navigationTitle("iExpense")
         }
-        .padding()
     }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+}
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+
+@Observable
+class Expense {
+    var items = [ExpenseItem]()
 }
 
 #Preview {
