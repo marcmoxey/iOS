9cb165a1e5d70b537c1332ad6414af84a7971c23
marcmoxey <moxeymarc@gmail.com>
2025-12-05 13:52:29 -0500

    Add decoded json, handle erros, better UI

---
+++ 02 - Phase/08Project/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Marc Moxey on 12/5/25.
+//
+
+import Foundation
+
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
